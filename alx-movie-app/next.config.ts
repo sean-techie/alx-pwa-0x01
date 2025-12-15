7d5acf118d0e4cef8ab72e3908bb947f33ccc735
Reseanne Rampou <reseanner@gmail.com>
2025-12-15 17:05:07 +0100

    Refactor PWA configuration in next.config.ts

--- alx-movie-app/next.config.ts
+++ alx-movie-app/next.config.ts
@@ -1,21 +1,19 @@
-
 import withPWAInit from "@ducanh2912/next-pwa";
 
 /** @type {import('next').NextConfig} */
+
 const withPWA = withPWAInit({
-  dest: 'public',                   // service worker output folder
-  register: true,                   // auto-register service worker
-  skipWaiting: true,                // activate new service worker immediately
-  disable: process.env.NODE_ENV === 'development', // disable PWA in dev
-});
+  dest: 'public'
+})
 
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['m.media-amazon.com'], // allow movie posters from this domain
+    domains: ['m.media-amazon.com'],
   },
 };
 
 export default withPWA({
-  ...nextConfig,
-});
+  ...nextConfig
+})
+
